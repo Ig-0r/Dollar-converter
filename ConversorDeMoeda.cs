456dbe01a9de442ed73591cb0db5de29959115dc
Igor Leite <igorlfilgueiras@gmail.com>
2024-08-05 20:46:26 -0300

    conversor de moeda basico, exercício de fixação

---
+++ ConversorDeMoeda.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDolarReal
+{
+    internal class ConversorDeMoeda
+    {
+        public static double IOF = 6.0;
+
+        public static double Converter(double cot, double quantia)
+        {
+            double total = cot * quantia;
+            return total + total * (IOF / 100.0);
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using ConversorDolarReal;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Qual a cotação atual do dólar? ");
+        double Cotação = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.Write("Quantos dólares deseja comprar? ");
+        double Quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        
+        
+        Console.Write("Valor a ser pago em reais = " + ConversorDeMoeda.Converter(Cotação, Quantia).ToString("F2", CultureInfo.InvariantCulture));
+    }
+
+
+}
